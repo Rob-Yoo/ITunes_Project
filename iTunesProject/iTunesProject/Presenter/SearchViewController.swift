c72601597eba69b92818e7f1778bb29bedc8f6c3
Rob-Yoo <jin12243@naver.com>
2024-08-08 18:28:28 +0900

    Chore: 프로젝트 세팅

---
+++ iTunesProject/iTunesProject/Presenter/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesProject
+//
+//  Created by Jinyoung Yoo on 8/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
